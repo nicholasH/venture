87f113345f7dc461bb44d05ae03d33c154e03c16
nicholasH <nicholas.hoover@hope.edu>
2015-09-23 18:28:20 +0000

    bootstrap

--- app/models/character.js
+++ app/models/character.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Item from './item';
 const BASE_HP = 40;
 const BASE_MANA = 30;
 
@@ -27,5 +28,19 @@ export default Ember.Object.extend({
   name: Ember.computed(function(){
     var names = ['Zultar', 'Zorky', 'Merlin'];
     return names[Math.floor(Math.random()*names.length)];
+  }),
+  itemWeights: Ember.computed.mapBy('items','weight'),
+  itemWeight: Ember.computed.sum('itemWeights'),
+  hampered: Ember.computed('itemWeight','maxWeight', function(){
+     return this.get('itemWeight') > this.get('maxWeight');
+  }),
+  maxWeight: Ember.computed('strength', function() {
+     return this.get('strength') * 5; 
+  }),
+
+  items: Ember.computed( function(){ 
+    return [Item.createRandom()];
   })
+
+
 });
