3d8e9ff483e9a8982ea5d9d44f04bd2ac0aea3d9
nicholasH <nicholas.hoover@hope.edu>
2015-12-01 22:09:53 +0000

    got test to work only if you start out loged out

--- app/routes/authenticated/character.js
+++ app/routes/authenticated/character.js
@@ -10,23 +10,7 @@ export default Ember.Route.extend({
 
 
 
-    return new Ember.RSVP.Promise((resolve,reject) =>{
-      return Ember.run.later(() => {
-        charp.then(chars => {
-
-          if(chars.get("length") > 0){
-            resolve(charp);
-            }
-           else{
-           reject(alert("there are no chars"));
-        }
-        });
-
-
-
-      }, 2000);
-
-    });
+    return charp;
 
 
 
--- tests/acceptance/characters-test.js
+++ tests/acceptance/characters-test.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from 'venture/tests/helpers/start-app';
+import Pretender from 'pretender';
+var H = window; // H is for helper
 
 module('Acceptance | characters', {
   beforeEach: function() {
@@ -8,14 +10,62 @@ module('Acceptance | characters', {
   },
 
   afterEach: function() {
+
+    if(server) {
+      server.shutdown();
+    }
+
     Ember.run(this.application, 'destroy');
   }
 });
 
-test('visiting /character', function(assert) {
+test('blocks characters when not logged in', function(assert) {
   visit('/authenticated/character');
 
   andThen(function() {
-    assert.equal(currentURL(), '/authenticated/character');
+    assert.equal(currentURL(), '/');
   });
+});
+
+Ember.Test.registerAsyncHelper('resumablePause', function() {
+  Ember.Test.adapter.asyncStart();
+  return new Ember.RSVP.Promise(function(resolve) {
+    window.continueTest = function() {
+      Ember.Test.adapter.asyncEnd();
+      resolve();
+    };
+  }, 'Test Adapter paused');
+});
+
+Ember.Test.registerAsyncHelper('seeOn', function(app, assert, url) {
+  assert.equal(currentURL(), url);
+});
+
+Ember.Test.registerAsyncHelper('loginAs', function(app, u, p) {
+  fillIn('input.app-email', u);
+  fillIn('input.app-password', p);
+  // click('.app-login-button')
+  click('button');
+});
+
+var server;
+test('can view characters when logged in', function(assert) {
+  server = new Pretender(function(){
+    this.get('/characters', function(){
+      return [200, {"Content-Type":"application/json"}, JSON.stringify(
+        [{}]
+      )];
+    });
+    this.post('/users/sign_in', function(){
+      return [201, {}, JSON.stringify({
+        token: 'someTokenString',
+        email: 'test@example.com'
+      })];
+    });
+  });
+
+  H.visit('/authenticated/character');
+  H.seeOn(assert, '/');
+  H.loginAs('test@example.com', 'asdfasdf');
+  H.seeOn(assert, '/authenticated/character');
 });
